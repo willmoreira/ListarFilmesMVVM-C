5d3258122a428d79de08624abebd8c062c5a9f4e
William Moreira <william.moreira3@fatec.sp.gov.br>
2023-06-15 11:59:15 -0300

    Ajuste de xcode mais antigo que o projeto, ajuste no SPM do firebase e ajuste do targetVersion do proejto

--- ListarFilmesMVVM-C/ViewController.swift
+++ ListarFilmesMVVM-C/ViewController.swift
@@ -6,14 +6,13 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad()      
         // Do any additional setup after loading the view.
     }
-
-
 }
 
